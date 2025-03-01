6e50252e745af60531df3f28af5863c88a463914
antking19 <cuong.truongkien@gmail.com>
2025-03-01 16:35:06 +0700

    video 52

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -5,15 +5,13 @@ const Header = () => {
     return (
         <ul>
             <li>
-                <NavLink class="active" to="/">
-                    Home
-                </NavLink>
+                <NavLink to="/">Home</NavLink>
             </li>
             <li>
                 <NavLink to="/users">Users</NavLink>
             </li>
             <li>
-                <NavLink to="/products">Products</NavLink>
+                <NavLink to="/books">Books</NavLink>
             </li>
         </ul>
     );
