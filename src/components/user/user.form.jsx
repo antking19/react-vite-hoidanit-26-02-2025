aec2517401f9d8212c83b785d5f16721ff85fb58
antking19 <cuong.truongkien@gmail.com>
2025-03-04 07:59:34 +0700

    video 69

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Input } from "antd";
-import { Button, Flex } from "antd";
+import { Input, Button } from "antd";
 import { useState } from "react";
+import axios from "axios";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -9,6 +9,15 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     const handleClickBtn = () => {
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone,
+        };
+
+        axios.post(URL_BACKEND, data);
         console.log(">>> check state: ", { fullName, email, password, phone });
     };
 
